97d1f7c25de8206c0254af97367f0fc1a91ab454
Andrew Andrijchuk <andrew.andrijchuk@wpt.dev>
2020-08-24 22:03:12 +0300

    page 'Category'

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./auth";
 import info from "./info";
+import category from "./category";
 
 Vue.use(Vuex);
 
@@ -29,5 +30,5 @@ export default new Vuex.Store({
   getters: {
     error: state => state.error
   },
-  modules: { auth, info }
+  modules: { auth, info, category }
 });
